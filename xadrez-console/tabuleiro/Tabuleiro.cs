bfb1c8704829a506a21deef96a54cd270ef9f63c
Matheus-Pedroso <matheuspedroso2012@gmail.com>
2023-08-27 10:57:47 -0300

    criando exceção personalizada de tabuleiro

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -1,4 +1,6 @@
-﻿namespace tabuleiro {
+﻿using tabuleiro;
+
+namespace tabuleiro {
     class Tabuleiro {
 
         public int linhas { get; set; }
@@ -15,9 +17,34 @@
             return pecas[linha, coluna];
         }
 
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
         public void colocarPeca(Peca p, Posicao pos) {
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+                return false;
+            return true;
+        }
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida!");
+            }
         }
     }
 }
